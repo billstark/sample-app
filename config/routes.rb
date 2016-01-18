816dde54c1d483bbdb949d12a00b999ebf03b278
Yang Zhuohan <billstark1996@gmail.com>
2016-01-18 21:17:04 +0800

    add a comment

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get  'help'    => 'static_pages#help'
   get  'about'   => 'static_pages#about'
   get  'contact' => 'static_pages#contact'
-  get  'signup'  => 'users#new'
+  get  'signup'  => 'users#new' #add new user controller
 
 end
